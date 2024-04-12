93d89efcbf7041daa9b54412784443092b976f3c
Karthikakalaivanan <124855777+23karthika@users.noreply.github.com>
2024-04-12 15:21:36 +0530

    Add files via upload

---
+++ seleniumdemo/src/test/java/com/selenium/seleniumdemo/SeleniumdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selenium.seleniumdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
